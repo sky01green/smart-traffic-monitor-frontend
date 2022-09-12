56b57be765ae3f51961844a7a152b69bceabd3fa
rumbleFTW <007rajdeepghosh@gmail.com>
2022-09-12 20:35:59 +0530

    Fixed video playback

--- assets/js/main.js
+++ assets/js/main.js
@@ -290,3 +290,30 @@ document.addEventListener('DOMContentLoaded', () => {
   });
 
 });
+
+const video = document.querySelector("video");
+let playState = null;
+
+const observer = new IntersectionObserver((entries) => {
+  entries.forEach((entry) => {
+    if (!entry.isIntersecting) {
+      video.pause();
+      playState = false;
+    } else {
+      video.play();
+      playState = true;
+    }
+  });
+}, {});
+
+observer.observe(video);
+
+const onVisibilityChange = () => {
+  if (document.hidden || !playState) {
+    video.pause();
+  } else {
+    video.play();
+  }
+};
+
+document.addEventListener("visibilitychange", onVisibilityChange);
